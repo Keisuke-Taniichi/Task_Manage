572208f345b8ca41541f9730183db3dee7b468ed
keisuke.taniichi <keisuke.taniichi@gmail.com>
2019-10-06 15:24:32 +0000

    C4

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   
-  resources :users
-  resources :tasks, only: :update
+  resources :users do 
+    member do 
+      get 'tasks/show'
+    end
+    resources :tasks, only: :update
+  end
 end
